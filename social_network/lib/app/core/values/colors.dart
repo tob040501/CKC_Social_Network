8aeb125983abaef00bd8e2581a511858a6d25cf9
TOB0451 <102936936+tob040501@users.noreply.github.com>
2022-05-14 08:33:39 +0700

    file main

---
+++ social_network/lib/app/core/values/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xFFFEA0C6);
+const Color iconColor = Color(0xffADADAD);
+const Color accentColor = Color(0xff181818);
+const Color yellowColor = Colors.yellow; //
+const Color blackColor = Colors.black;
